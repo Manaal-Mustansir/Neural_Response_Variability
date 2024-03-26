7529defcf6844685b20fcf7cca1fcfd60ddee8de
Manaal-Mustansir <145365385+Manaal-Mustansir@users.noreply.github.com>
2024-03-26 13:59:06 -0500

    Each Cluster Raster in SVG

--- scripts/Raster_Plots_FR.py
+++ scripts/Raster_Plots_FR.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load spike times, clusters, and trial DataFrame
 spike_times_secpath = utils.getFilePath(windowTitle="Spike_times", filetypes=[('Spike times numpy file', '*.npy')])
 clusters_path = utils.getFilePath(windowTitle="Select_clusters", filetypes=[('Clusters numpy file', '*.npy')])
 trial_DF_path = utils.getFilePath(windowTitle="trials_DF_Fixation", filetypes=[('Trials_Fixation csv file', '*.csv')])
@@ -12,30 +11,30 @@ spike_times_sec = np.load(spike_times_secpath)
 clusters = np.load(clusters_path)
 trialDF = pd.read_csv(trial_DF_path)
 
-# Extract spike times for each cluster
 spike_times_clusters = utils.get_spike_times(clusters, spike_times_sec)
 
-# Extract data for the first three trials
-trialDF_first_three = trialDF.head(3)
+def plot_raster(Y, start_times, stop_times, firing_rates, cluster_index):
+    fig, ax = plt.subplots()
+    for trial, (start_time, stop_time, firing_rate) in enumerate(zip(start_times, stop_times, firing_rates)):
+        spikes_in_trial = Y[(Y >= start_time - 0.2) & (Y <= stop_time)]
+        ax.eventplot(spikes_in_trial - start_time, color='black', linewidths=0.5, lineoffsets=trial+1)
+    ax.set_title(f'Raster Plot - Cluster {cluster_index}')
+    ax.set_xlabel('Time (s)')
+    ax.set_ylabel('Trial')
+    plt.savefig(f'Cluster_{cluster_index}_raster.svg')  # Save the raster plot for each cluster
+    plt.close()  # Close the plot to avoid overlapping
 
-max_clusters = 250
-skip_clusters = 50
+def calculate_mean_firing_rate(Y, trialDF, i):
+    start_times = trialDF['fixationstart']
+    stop_times = trialDF['fixationstop']
+    firing_rates = []
 
-fix, ax = plt.subplots(figsize=(10, (max_clusters - skip_clusters) * 0.025))
+    for start_time, stop_time in zip(start_times, stop_times):
+        spikes_in_window = Y[(Y >= start_time - 0.2) & (Y <= stop_time)]
+        firing_rate = len(spikes_in_window) / (stop_time - start_time) if stop_time > start_time else 0
+        firing_rates.append(firing_rate)
 
-# Iterate over clusters, skipping the first 50 clusters
-for i, (cluster_index, Y) in enumerate(list(spike_times_clusters.items())[skip_clusters:max_clusters + skip_clusters]):
-    for index, row in trialDF_first_three.iterrows():
-        start_time = row['fixationstart']
-        stop_time = row['fixationstop']
+    plot_raster(Y, start_times, stop_times, firing_rates, i)
 
-        # Filter spike times within the trial window (5s to 10s)
-        spikes_in_trial = Y[(Y >= start_time + 5) & (Y <= start_time + 10)]
-
-        # Plot raster plot
-        ax.eventplot(spikes_in_trial - start_time, color='black', linewidths=0.5, lineoffsets=i+1)
-
-ax.set_xlabel('Time (s)')
-ax.set_ylabel('Clusters')
-ax.set_title('Clusters')
-plt.show()
+for i, Y in spike_times_clusters.items():
+    calculate_mean_firing_rate(Y, trialDF, i)
