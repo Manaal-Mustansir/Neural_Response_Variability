f646018aa83667b6403102f87a0b74549bb862fb
Manaal-Mustansir <145365385+Manaal-Mustansir@users.noreply.github.com>
2024-04-23 15:40:15 -0500

    Stimuli_Raster

---
+++ scripts/Preprocessing.py
@@ -0,0 +1,80 @@
+from lib import readSGLX
+from utils import utils
+import numpy as np
+import subprocess 
+import pandas as pd
+from time import sleep
+import os
+
+CatGT_dir  = 'C:/Users/mmustans/Documents/GitHub/CatGTWinApp/CatGT-win/'
+TPrime_dir = 'C:/Users/mmustans/Documents/GitHub/TPrimeWinApp/TPrime-win/'
+
+# catGT parameters
+DIR = ' -dir=C:/Users/mmustans/Documents/Data/MM001_Sansa/Electrophysiology/2024-04-18'
+RUN = ' -run=BSD'
+prs = ' -g=0 -t=0 -ap -ni -prb_fld -prb=0'
+xa2 = ' -xa=0,0,3,2.5,1,0'  # trial start
+xia = ' -xia=0,0,3,2.5,3.5,0'  # trial stop
+xa = ' -xa=0,0,5,2.5,1,0'  # stim start
+xia2 = ' -xia=0,0,5,2.5,3,0'  # stim stop
+
+# run CatGT
+os.chdir('C:/Users/mmustans/Documents/GitHub/CatGTWinApp/CatGT-win/')
+print('CatGT is running, please wait for the process to finish')
+subprocess.run('CatGT' + DIR + RUN + prs + xia + xa + xa2 + xia2, shell=True, capture_output=True)
+
+# convert spike times to seconds
+binFullPath = utils.getFilePath(windowTitle="Select binary ap file", filetypes=[("sGLX binary", "*.bin")])
+spikesFullPath = utils.getFilePath(windowTitle="Select spikes file", filetypes=[("KS output spikes_times", "*.npy")])
+meta = readSGLX.readMeta(binFullPath)
+sRate = readSGLX.SampRate(meta)
+spike_times_smp = np.load(spikesFullPath)
+spike_times_sec = np.around(np.divide(spike_times_smp, sRate, dtype=float), decimals=6)
+np.save(spikesFullPath.with_stem('spike_times_sec'), spike_times_sec)
+
+# TPrime parameters
+tostream = utils.getFilePath(windowTitle="SYNC tostream file (IMEC0 edgefile.txt)", filetypes=[("CatGT output", "*.txt")])
+fromstream = utils.getFilePath(windowTitle="SYNC fromstream file (usually NIDAQ edgefile.txt)", filetypes=[("CatGT output", "*.txt")])
+trialstart = utils.getFilePath(windowTitle="SYNC trial start (usually xa2)", filetypes=[("CatGT output", "*.txt")])
+trialstop = utils.getFilePath(windowTitle="SYNC trial stop (usually xia2)", filetypes=[("CatGT output", "*.txt")])
+stimstart = utils.getFilePath(windowTitle="SYNC stimulus start (usually xa4)", filetypes=[("CatGT output", "*.txt")])
+stimstop = utils.getFilePath(windowTitle="SYNC stimulus stop (usually xa5)", filetypes=[("CatGT output", "*.txt")])
+
+syncperiod = ' -syncperiod=1.0'
+tostream = ' -tostream=' + str(tostream)
+fromstream = ' -fromstream=1,' + str(fromstream)
+trialstart = ' -events=1,' + str(trialstart) + ',' + str(trialstart)[0:len(str(trialstart)) - len(str(trialstart.stem) + '.txt')] + 'trialstart.txt'
+trialstop = ' -events=1,' + str(trialstop) + ',' + str(trialstop)[0:len(str(trialstop)) - len(str(trialstop.stem) + '.txt')] + 'trialstop.txt'
+stimstart = ' -events=1,' + str(stimstart) + ',' + str(stimstart)[0:len(str(stimstart)) - len( str(stimstart.stem) + '.txt')] + 'stimstart.txt'
+stimstop = ' -events=1,' + str(stimstop) + ',' + str(stimstop)[0:len(str(stimstop)) - len(str(stimstop.stem) + '.txt')] + 'stimstop.txt'
+
+# run TPrime 
+subprocess.run('cd ' + TPrime_dir, shell=True)
+subprocess.run('TPrime' + syncperiod + tostream + fromstream + trialstart + trialstop + stimstart + stimstop + stimstop, shell=True)
+
+# get paths to the pulse files 
+trialstartFullPath = utils.getFilePath(windowTitle="Select trialstart file", filetypes=[("TPrime output", "*.txt")])
+trialstopFullPath = utils.getFilePath(windowTitle="Select trialstop file", filetypes=[("TPrime output", "*.txt")])
+stimstartFullPath = utils.getFilePath(windowTitle="Select stimstart file", filetypes=[("TPrime output", "*.txt")])
+stimstopFullPath = utils.getFilePath(windowTitle="Select stimstop file", filetypes=[("TPrime output", "*.txt")])
+
+# read the pulse files and convert to dataframe
+trialstartDF = pd.read_csv(trialstartFullPath.absolute(), sep=" ", header=None)
+trialstartDF.columns = ['trialstart']
+
+trialstopDF = pd.read_csv(trialstopFullPath.absolute(), sep=" ", header=None)
+trialstopDF.columns = ['trialstop']
+
+trialsDF = trialstartDF.join(trialstopDF)
+
+stimstartDF = pd.read_csv(stimstartFullPath.absolute(), sep=" ", header=None)
+stimstartDF.columns = ['stimstart']
+
+stimstopDF = pd.read_csv(stimstopFullPath.absolute(), sep=" ", header=None)
+stimstopDF.columns = ['stimstop']
+stimstopDF['stimstop'] = stimstartDF['stimstart'] + 0.5
+
+# save the dataframes
+trialsDF.to_csv(trialstartFullPath.with_suffix('.csv'), index=False)
+stimstartDF.to_csv(stimstartFullPath.with_suffix('.csv'), index=False)
+stimstopDF.to_csv(stimstopFullPath.with_suffix('.csv'), index=False)
--- scripts/Raster_Plots_FR.py
+++ scripts/Raster_Plots_FR.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 spike_times_secpath = utils.getFilePath(windowTitle="Spike_times", filetypes=[('Spike times numpy file', '*.npy')])
 clusters_path = utils.getFilePath(windowTitle="Select_clusters", filetypes=[('Clusters numpy file', '*.npy')])
-trial_DF_path = utils.getFilePath(windowTitle="trials_DF_Fixation", filetypes=[('Trials_Fixation csv file', '*.csv')])
+trial_DF_path = utils.getFilePath(windowTitle="trials_DF_Stimulus", filetypes=[('Trials_Stimulus csv file', '*.csv')])
 
 spike_times_sec = np.load(spike_times_secpath)
 clusters = np.load(clusters_path)
@@ -13,28 +13,58 @@ trialDF = pd.read_csv(trial_DF_path)
 
 spike_times_clusters = utils.get_spike_times(clusters, spike_times_sec)
 
-def plot_raster(Y, start_times, stop_times, firing_rates, cluster_index):
-    fig, ax = plt.subplots()
-    for trial, (start_time, stop_time, firing_rate) in enumerate(zip(start_times, stop_times, firing_rates)):
-        spikes_in_trial = Y[(Y >= start_time - 0.2) & (Y <= stop_time)]
-        ax.eventplot(spikes_in_trial - start_time, color='black', linewidths=0.5, lineoffsets=trial+1)
+
+def plot_raster(Y, start_time, stop_time, firing_rate, cluster_index, ax, trial):
+    # Plot raster plot
+    ax.eventplot(Y - start_time, color='black', linewidths=0.5, lineoffsets=trial+1)
     ax.set_title(f'Raster Plot - Cluster {cluster_index}')
     ax.set_xlabel('Time (s)')
     ax.set_ylabel('Trial')
-    plt.savefig(f'Cluster_{cluster_index}_raster.svg')  # Save the raster plot for each cluster
-    plt.close()  # Close the plot to avoid overlapping
+
+    # Plot firing rate on a separate axis
+    """ ax1 = plt.gca().twinx()
+    ax1.plot([start_time, stop_time], [firing_rate, firing_rate], color='red', linestyle='dashed', linewidth=2)
+    ax1.set_ylabel('Firing Rate (spikes/s)', color='red')
+    ax1.tick_params(axis='y', labelcolor='red') """
+
 
 def calculate_mean_firing_rate(Y, trialDF, i):
-    start_times = trialDF['fixationstart']
-    stop_times = trialDF['fixationstop']
-    firing_rates = []
+    firing_rates = []  # List to store firing rates for each trial
+    fig, ax = plt.subplots()
+    for index, row in trialDF.iterrows():
+        start_time = row['stimstart']
+        stop_time = row['stimstop']
 
-    for start_time, stop_time in zip(start_times, stop_times):
-        spikes_in_window = Y[(Y >= start_time - 0.2) & (Y <= stop_time)]
-        firing_rate = len(spikes_in_window) / (stop_time - start_time) if stop_time > start_time else 0
+        # Filter spike times within the trial window
+        spikes_in_trial = Y[(Y >= start_time - 0.2) & (Y <= stop_time)]
+
+        # Calculate firing rate for the trial
+        firing_rate = len(spikes_in_trial) / (stop_time - start_time) if stop_time > start_time else 0
+
+        # Update the firing rates list
         firing_rates.append(firing_rate)
 
-    plot_raster(Y, start_times, stop_times, firing_rates, i)
+        # Calculate baseline window
+        baseline_start = start_time - 0.2  # 200ms before stimulus start
+        baseline_end = start_time
 
-for i, Y in spike_times_clusters.items():
-    calculate_mean_firing_rate(Y, trialDF, i)
+        # Update the DataFrame
+        trialDF.at[index, f'baseline_spike_count_cluster_{i}'] = len(spikes_in_trial)
+        trialDF.at[index, f'baseline_firing_rate_cluster_{i}'] = len(spikes_in_trial) / (baseline_end - baseline_start) if baseline_end > baseline_start else 0
+        trialDF.at[index, f'evoked_response_cluster_{i}'] = firing_rate
+
+        # Pass firing_rate as an argument to the plot_raster function
+        plot_raster(spikes_in_trial, start_time, stop_time, firing_rate, i, ax, trial=index)
+
+    plt.show()
+
+    return trialDF
+
+for i in spike_times_clusters.keys():
+    Y = spike_times_clusters[i]
+
+    # Calculate mean firing rate, baseline, and evoked response for each cluster
+    trialDF = calculate_mean_firing_rate(Y, trialDF, i)
+
+# Display the updated DataFrame
+print(trialDF)
