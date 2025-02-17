8e5b84f3607f97f60af1a24e6086ce8892cc2007
Mustansir <mmustans@CougarNet.UH.EDU>
2025-02-17 09:20:43 -0600

    FRMeanMatch

--- scripts/FF_meanmatching.py
+++ scripts/FF_meanmatching.py
@@ -12,6 +12,13 @@ import statsmodels.api as sm
 # Results directory
 results_dir = "C:/Users/mmustans/Documents/Projects/Neural_Response_Variability/results"
 
+csv_file_path = utils.getFilePath(
+    windowTitle="Select CSV File",
+    filetypes=[("CSV files", "*.csv")]
+)
+
+# Load the selected CSV file
+df = pd.read_csv(csv_file_path)
 # Constants for penetration date and monkey name
 penetration_date = '2024'
 monkey_name = 'Sansa'
@@ -55,7 +62,7 @@ def mean_match(mean1, mean2, variance1, variance2, count_bins, nboots=100):
 
         slope1[boot], slope2[boot] = results1.params[0], results2.params[0]
 
-    return slope1, slope2, meantmp1, meantmp2, inds1_final, inds2_final
+    return slope1, slope2, meantmp1, meantmp2, inds1_final, inds2_final, 
 
 
 # Spike counting function for baseline and evoked responses
@@ -121,8 +128,10 @@ for dataset_num in range(1, 2):
 
     # Get spike times clustered
     spike_times_clusters = utils.get_spike_times(clusters, spike_times_sec)
-    spkC_low, spkC_high, FF_low, FF_high = [], [], [], []
-
+    spkC_low, spkC_high, FF_low, FF_high, FR_low, FR_high = [], [], [], [], [], []
+    pre_time = 0.15
+    post_time = 0.15
+    initial_time = 0.035
     for c in spike_times_clusters.keys():
         Y = spike_times_clusters[c]
         
@@ -142,19 +151,22 @@ for dataset_num in range(1, 2):
                 FF_low.append(np.var(evoked_counts_low) / np.mean(evoked_counts_low) if np.mean(evoked_counts_low) > 0 else np.nan)
                 FF_high.append(np.var(evoked_counts_high) / np.mean(evoked_counts_high) if np.mean(evoked_counts_high) > 0 else np.nan)
                 spkC_low.append(np.mean(evoked_counts_low))
+                FR_low.append(np.mean(evoked_counts_low) / post_time)
+                FR_high.append(np.mean(evoked_counts_high) / post_time)
                 spkC_high.append(np.mean(evoked_counts_high))
+
 
     # Mean Matching for Fano Factors
     spkC_low, spkC_high, FF_low, FF_high = np.array(spkC_low), np.array(spkC_high), np.array(FF_low), np.array(FF_high)
     count_bins = np.arange(0, 301, 1)
     slope1, slope2, mean_low_matched, mean_high_matched, matched_inds_low, matched_inds_high = mean_match(
     spkC_low, spkC_high, FF_low, FF_high, count_bins)
-
+    
     # Plot  Fano factors
     plt.figure(figsize=(18, 6))
 
     # Fano Factor scatter plot
-    plt.subplot(1, 3, 1)
+    plt.subplot(1, 5, 1)
     plt.plot(FF_low, FF_high, 'ko', markersize=3, markerfacecolor='None')
     plt.plot([0.5, 8], [0.5, 8], 'k-')
     plt.xscale('log')
@@ -163,7 +175,7 @@ for dataset_num in range(1, 2):
     plt.ylabel('Fano Factor High')
    
     # Matched Fano Factor bar plot
-    plt.subplot(1, 3, 2)
+    plt.subplot(1, 5, 2)
     # Get the number of units for each condition
     n_low = len(FF_low)  # Count of values n
     n_high = len(FF_high)
@@ -196,16 +208,47 @@ for dataset_num in range(1, 2):
     FF_high_SE = sem(FF_high, nan_policy='omit')
 
    # Plot Raw Fano Factor
-    plt.subplot(1, 3, 3)
+    plt.subplot(1, 5, 3)
     plt.bar([1, 2], [FF_low_mean, FF_high_mean], yerr=[FF_low_SE, FF_high_SE], color=['gray', 'red'], capsize=5)
     plt.xticks([1, 2], ['Low', 'High'])
     plt.ylabel('Fano Factor Raw')
     plt.text(1, FF_low_mean + 0.1, f"n={int(n_low)}", ha='center', fontsize=6, fontweight='bold')
     plt.text(2, FF_high_mean + 0.1, f"n={int(n_high)}", ha='center', fontsize=6, fontweight='bold')
+    
+    # Plot for firing rates
+    mean_low = np.nanmean(df["Evoked Low Firing Rate"])
+    mean_high = np.nanmean(df["Evoked High Firing Rate"])
+    stderr_low = sem(df["Evoked Low Firing Rate"], nan_policy='omit')
+    stderr_high = sem(df["Evoked High Firing Rate"], nan_policy='omit')
+    plt.subplot(1, 5, 4)
+    plt.bar(['Low', 'High'], [mean_low, mean_high], 
+        yerr=[stderr_low, stderr_high], 
+        color=['gray', '#FF0000'], capsize=5, edgecolor='black', linewidth=3)
+    plt.ylabel('Firing Rate (Hz)')
+    plt.xlabel('Pupil Diameter')
 
+
+
+
+    # Plot for Mean-matched firing rates
+    FR_low_matched = np.array(FR_low)[matched_inds_low]
+    FR_high_matched = np.array(FR_high)[matched_inds_high]
+    # matched indices
+    FR_low_mean_matched, FR_high_mean_matched = np.nanmean(FR_low_matched), np.nanmean(FR_high_matched)
+    FR_low_matched_SE, FR_high_matched_SE = sem(FR_low_matched), sem(FR_high_matched)
+    n_matched_low_FR = len(FR_low_matched)  # Number of units in matched condition
+    n_matched_high_FR = len(FR_high_matched)
+    plt.subplot(1, 5, 5)
+    
+    plt.bar([1, 2], [FR_low_mean_matched, FR_high_mean_matched], yerr=[FR_low_matched_SE, FR_high_matched_SE], color=['gray', 'red'], capsize=5)
+    plt.xticks([1, 2], ['Low', 'High'])
+    plt.ylabel('Mean-matched Firing Rate')
+    # Display `n` values above bars for Mean-Matched FR
+    plt.text(1, FR_low_mean_matched + 0.2, f"n={n_matched_low_FR}", ha='center', fontsize=8, fontweight='bold')
+    plt.text(2, FR_high_mean_matched + 0.2, f"n={n_matched_high_FR}", ha='center', fontsize=8, fontweight='bold')
+    print("Matched FR_low:", FR_low_mean_matched)
+    print("Matched FR_high:", FR_high_mean_matched)
     # Save and show plot
-    plt.savefig(os.path.join(results_dir, 'fano_factor.svg'))
+    plt.savefig(os.path.join(results_dir, 'Penetration-1..svg'))
     plt.show()
-
-
 
